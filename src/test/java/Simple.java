9b565d369417f6ceb173ed32acd9ab872e03f905
Kolasfg <danydann@yandex.ru>
2022-09-13 18:07:34 +0300

    init commit

---
+++ src/test/java/Simple.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Simple {
+    @Test
+    public void example(){
+        Assertions.assertTrue(34<45);
+    }
+}
